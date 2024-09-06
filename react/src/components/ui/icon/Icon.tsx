66c61a5daf47dee793eab0cfbf697446ebc642ba
Cole Chrzan <cole@schematichq.com>
2024-09-06 13:12:53 -0400

    update deps

--- react/src/components/ui/icon/Icon.tsx
+++ react/src/components/ui/icon/Icon.tsx
@@ -2,6 +2,8 @@ import cx from "classnames";
 import icons from "../../../assets/icons/icons.js";
 import { Icon as StyledIcon } from "./styles";
 
+import "../../../assets/icons/icons.css";
+
 export type IconNameTypes = keyof typeof icons;
 
 export interface IconProps extends React.HTMLAttributes<HTMLElement> {
