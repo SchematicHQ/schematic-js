5a95bcc853236306630e8674352078f2db73b086
youssefsh <ussefo@gmail.com>
2024-09-06 18:06:27 +0300

    SCH-1563 Disabled state ui

--- react/src/components/layout/viewport/Viewport.tsx
+++ react/src/components/layout/viewport/Viewport.tsx
@@ -1,6 +1,7 @@
 import { forwardRef } from "react";
 import { useEmbed } from "../../../hooks";
 import { StyledViewport } from "./styles";
+import { Box, Flex } from "../../ui";
 
 export interface ViewportProps extends React.HTMLProps<HTMLDivElement> {}
 
@@ -14,7 +15,29 @@ export const Viewport = forwardRef<HTMLDivElement | null, ViewportProps>(
         $numberOfColumns={settings.theme.numberOfColumns}
         {...props}
       >
-        {layout === "disabled" ? <div className="">DISABLED</div> : children}
+        {layout === "disabled" ? (
+          <Box $width="100%">
+            <Flex
+              $flexDirection="column"
+              $padding="6rem"
+              $width="100%"
+              $height="auto"
+              $borderRadius=".5rem"
+              $backgroundColor="#F4F4F4"
+              $alignItems="center"
+              $justifyContent="center"
+            >
+              <Box $fontSize="37px" $marginBottom="8px" $fontWeight="600">
+                Coming soon
+              </Box>
+              <Box $fontSize="16px">
+                The plan manager will be back very soon.
+              </Box>
+            </Flex>
+          </Box>
+        ) : (
+          children
+        )}
       </StyledViewport>
     );
   },
