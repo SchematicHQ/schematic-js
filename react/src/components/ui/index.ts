62268f256f19f00d88da2af3bd8a1ed3bd49fa74
Ben Papillon <benpapillon@gmail.com>
2024-09-17 11:58:12 -0400

    Export loader component

--- react/src/components/ui/index.ts
+++ react/src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from "./box";
 export * from "./button";
 export * from "./flex";
 export * from "./icon";
+export * from "./loader";
 export * from "./modal";
 export * from "./progress-bar";
 export * from "./text";
