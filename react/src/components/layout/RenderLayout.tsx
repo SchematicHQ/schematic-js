1594e6826799699b7a2fb730d265f2f365a6314f
Cole Chrzan <cole@schematichq.com>
2024-09-18 14:15:37 -0400

    remove modal

--- react/src/components/layout/RenderLayout.tsx
+++ react/src/components/layout/RenderLayout.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useTheme } from "styled-components";
 import { useEmbed } from "../../hooks";
 import { ReactNode, useEffect } from "react";
-import { Box, Flex, IconRound, Modal, ModalHeader, Text } from "../ui";
+import { Box, Flex, IconRound, Text } from "../ui";
 import { TEXT_BASE_SIZE } from "../../const";
 
 const DisabledState = () => {
@@ -63,45 +63,45 @@ const SuccessState = () => {
   }, [isPending, isOpen, setLayout]);
 
   return (
-    <Modal>
-      <ModalHeader />
-
-      <Flex
-        $flexDirection="column"
-        $justifyContent="center"
-        $alignItems="center"
-        $marginTop="-5rem"
-        $flexGrow="1"
-        $gap={`${32 / TEXT_BASE_SIZE}rem`}
-        $padding={`${theme.card.padding / TEXT_BASE_SIZE}rem ${(theme.card.padding * 1.5) / TEXT_BASE_SIZE}rem`}
-        $whiteSpace="nowrap"
+    <Flex
+      $flexDirection="column"
+      $justifyContent="center"
+      $alignItems="center"
+      $gap={`${32 / TEXT_BASE_SIZE}rem`}
+      $width="min-content"
+      $height="min-content"
+      $margin="auto"
+      $padding={`${theme.card.padding / TEXT_BASE_SIZE}rem ${(theme.card.padding * 1.5) / TEXT_BASE_SIZE}rem`}
+      $whiteSpace="nowrap"
+      $backgroundColor={theme.card.background}
+      $borderRadius="0.5rem"
+      $boxShadow="0px 1px 20px 0px #1018280F, 0px 1px 3px 0px #1018281A;"
+    >
+      <IconRound
+        name="check"
+        size="3xl"
+        colors={[theme.card.background, theme.primary]}
+      />
+      <Text
+        as="h1"
+        $font={theme.typography.heading1.fontFamily}
+        $size={theme.typography.heading1.fontSize}
+        $weight={theme.typography.heading1.fontWeight}
+        $color={theme.typography.heading1.color}
       >
-        <IconRound
-          name="check"
-          size="3xl"
-          colors={[theme.card.background, theme.primary]}
-        />
-        <Text
-          as="h1"
-          $font={theme.typography.heading1.fontFamily}
-          $size={theme.typography.heading1.fontSize}
-          $weight={theme.typography.heading1.fontWeight}
-          $color={theme.typography.heading1.color}
-        >
-          Subscription updated!
-        </Text>
+        Subscription updated!
+      </Text>
 
-        <Text
-          as="p"
-          $font={theme.typography.text.fontFamily}
-          $size={theme.typography.text.fontSize}
-          $weight={theme.typography.text.fontWeight}
-          $color={theme.typography.text.color}
-        >
-          Loading...
-        </Text>
-      </Flex>
-    </Modal>
+      <Text
+        as="p"
+        $font={theme.typography.text.fontFamily}
+        $size={theme.typography.text.fontSize}
+        $weight={theme.typography.text.fontWeight}
+        $color={theme.typography.text.color}
+      >
+        Loading...
+      </Text>
+    </Flex>
   );
 };
 
