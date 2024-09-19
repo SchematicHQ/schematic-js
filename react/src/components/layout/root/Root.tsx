ab51779a90dc6db52c9cfae05af6579ccf9dbca9
Cole Chrzan <cole@schematichq.com>
2024-09-19 01:44:15 -0400

    render fixes and version up

--- react/src/components/layout/root/Root.tsx
+++ react/src/components/layout/root/Root.tsx
@@ -1,9 +1,15 @@
 import { forwardRef } from "react";
+import type { ComponentHydrateResponseData } from "../../../api";
+import { type EmbedSettings } from "../../../context";
 
-export interface RootProps extends React.HTMLProps<HTMLDivElement> {}
+export interface RootProps
+  extends Omit<React.HTMLProps<HTMLDivElement>, "data"> {
+  data?: ComponentHydrateResponseData;
+  settings?: EmbedSettings;
+}
 
 export const Root = forwardRef<HTMLDivElement | null, RootProps>(
-  (props, ref) => {
+  ({ data, settings, ...props }, ref) => {
     return <div ref={ref} {...props} />;
   },
 );
