106d9d05cfa2f116da25d623545a2b91d48a73b7
Cole Chrzan <cole@schematichq.com>
2024-10-22 13:34:47 -0400

    included features updates

--- components/src/components/elements/metered-features/MeteredFeatures.tsx
+++ components/src/components/elements/metered-features/MeteredFeatures.tsx
@@ -106,7 +106,18 @@ export const MeteredFeatures = forwardRef<
     [],
   );
 
-  if (featureUsage.length === 0) {
+  // Check if we should render this component at all:
+  // * If there are any plans or add-ons, render it, even if the list is empty.
+  // * If there are any features, show it (e.g., there could be features available via company overrides
+  //  even if the company has no plan or add-ons).
+  // * If none of the above, don't render the component.
+  const shouldShowFeatures =
+    featureUsage.length > 0 ||
+    data.company?.plan ||
+    (data.company?.addOns ?? []).length > 0 ||
+    false;
+
+  if (!shouldShowFeatures) {
     return null;
   }
 
