07c401756a37a8cf74f4d434d0375dd38a92c76c
Joseph Chrzan <cole@schematichq.com>
2025-01-14 13:08:29 -0500

    sch-2807 (#217)
    * allocation or quantity for quantity
    
    * update || to ??

--- components/src/components/shared/checkout-dialog/CheckoutDialog.tsx
+++ components/src/components/shared/checkout-dialog/CheckoutDialog.tsx
@@ -118,7 +118,12 @@ export const CheckoutDialog = ({ top = 0 }: CheckoutDialogProps) => {
         const allocation = featureUsage?.allocation || 0;
         const usage = featureUsage?.usage || 0;
 
-        acc.push({ usageData, allocation, quantity: allocation, usage });
+        acc.push({
+          usageData,
+          allocation,
+          quantity: allocation ?? usage,
+          usage,
+        });
 
         return acc;
       },
@@ -146,7 +151,12 @@ export const CheckoutDialog = ({ top = 0 }: CheckoutDialogProps) => {
           );
           const allocation = featureUsage?.allocation || 0;
           const usage = featureUsage?.usage || 0;
-          acc.push({ entitlement, allocation, quantity: allocation, usage });
+          acc.push({
+            entitlement,
+            allocation,
+            quantity: allocation ?? usage,
+            usage,
+          });
         }
 
         return acc;
