8395aebf0ca3a793461783b09221ef4036cb663f
Cole Chrzan <cole@schematichq.com>
2025-03-28 13:36:47 -0400

    fix(sch-3185): infinite rendering loop and css export

--- components/src/components/ui/icon/Icon.tsx
+++ components/src/components/ui/icon/Icon.tsx
@@ -1,17 +1,1 @@
-// New file
-import "schematic-icons/style.css";
-
-import { Icon as IconComponent, IconNames, IconProps } from "schematic-icons";
-
-import * as styles from "./styles";
-
-export type IconNameTypes = IconNames;
-
-export const Icon = ({ name, style, className, ...props }: IconProps) => {
-  console.log("Styles", style);
-  return (
-    <styles.Icon name={name}>
-      <IconComponent name={name} className={className} {...props} />
-    </styles.Icon>
-  );
-};
+export * from "./styles";
