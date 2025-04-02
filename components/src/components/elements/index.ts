8f6e72167e50ae0c95a62de376e7bf39f3262c5e
dmtrmrtnv <dimonmartyn@gmail.com>
2025-04-02 15:27:21 +0300

    SCH-1929: Add text element.

--- components/src/components/elements/index.ts
+++ components/src/components/elements/index.ts
@@ -4,5 +4,6 @@ export * from "./metered-features";
 export * from "./payment-method";
 export * from "./plan-manager";
 export * from "./pricing-table";
+export * from "./text";
 export * from "./unsubscribe-button";
 export * from "./upcoming-bill";
