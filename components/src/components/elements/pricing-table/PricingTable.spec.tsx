c4f5566f9b841ea2e939062760064712fd4a86fc
Cole Chrzan <cole@schematichq.com>
2025-04-05 01:29:17 -0400

    chore: initial setup

---
+++ components/src/components/elements/pricing-table/PricingTable.spec.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/experimental-ct-react";
+
+import { EmbedProvider } from "../../../context";
+import { PricingTable } from ".";
+
+test("should work", async ({ mount }) => {
+  const component = await mount(
+    <EmbedProvider>
+      <PricingTable />
+    </EmbedProvider>,
+  );
+  await expect(component).toContainText("Plans");
+});
