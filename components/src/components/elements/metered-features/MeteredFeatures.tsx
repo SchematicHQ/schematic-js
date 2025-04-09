349350b946f086bdfbb9ce48f08acf78e2309d40
Vladyslav Len <heavenly.beta@gmail.com>
2025-04-09 19:07:05 +0200

    fix(): overage prices decimals

--- components/src/components/elements/metered-features/MeteredFeatures.tsx
+++ components/src/components/elements/metered-features/MeteredFeatures.tsx
@@ -194,20 +194,6 @@ export const MeteredFeatures = forwardRef<
             }
           }
 
-          // Overage price must be derived from the subscription object
-          if (isOverage) {
-            const productId = (yearlyUsageBasedPrice ?? monthlyUsageBasedPrice)!
-              .productId;
-            if (productId) {
-              const products = data?.subscription?.products ?? [];
-              const product = products.find((p) => p.id === productId);
-              if (product) {
-                price = product.price;
-                currency = product.currency;
-              }
-            }
-          }
-
           const progressBar = props.isVisible &&
             typeof usage === "number" &&
             limit > 0 &&
