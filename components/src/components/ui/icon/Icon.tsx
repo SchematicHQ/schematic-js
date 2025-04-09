dbf2dde3072eb7271a051457416f9d91e6662222
Cole Chrzan <cole@schematichq.com>
2025-04-09 16:28:12 -0400

    Revert "[SCH-3185] Standalone icon package integration, initial start (#301)"
    This reverts commit 23ff945a10a087827906637d1cad105f40fda7d2.

--- components/src/components/ui/icon/Icon.tsx
+++ components/src/components/ui/icon/Icon.tsx
@@ -1,1 +1,14 @@
-export * from "./styles";
+import cx from "classnames";
+
+import icons from "./icons.js";
+import * as styles from "./styles";
+
+export type IconNameTypes = keyof typeof icons;
+
+export interface IconProps extends React.HTMLAttributes<HTMLElement> {
+  name: IconNameTypes;
+}
+
+export const Icon = ({ name, className, ...props }: IconProps) => {
+  return <styles.Icon className={cx("i", `i-${name}`, className)} {...props} />;
+};
