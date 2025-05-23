4f4e0b9afa94aa5fb5cd1602c8190a8ea8a1f352
Mohammed Almusaddar <my.almusaddar@gmail.com>
2025-05-23 22:18:05 +0300

    sch-3498-set-up-preview-apps-for-schematic-components
    Add GitHub Actions workflow for Vercel deployment and local testing script
    
    - Create a new workflow to clone a demo app and deploy components to Vercel on pull requests.
    - Implement a script for local testing of components and demo app with options for local or Vercel testing.

---
+++ scripts/test-components.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# scripts/test-components-locally.sh
+
+VERCEL_SCOPE="team_5rA0CJ7KWwUQut8p773DLhUz" # Schematic Vercel scope
+
+# ask user to test locally or on Vercel
+read -p "Do you want to test locally or on Vercel? (local/vercel): " choice
+
+if [ "$choice" != "local" ] && [ "$choice" != "vercel" ]; then
+    echo "Invalid choice. Please enter 'local' or 'vercel'."
+    exit 1
+fi
+
+# build components
+echo "🔨 Building components..."
+cd ../components || exit 1
+yarn install
+yarn build
+
+echo "📦 Linking components..."
+yarn link
+
+echo "🏠 Navigating to demo app..."
+cd ../../schematic-next-example || exit 1
+
+echo "🔗 Linking components to demo app..."
+yarn link "@schematichq/schematic-components"
+
+echo "🏗️ Installing dependencies..."
+yarn install --force
+
+if [ "$choice" == "local" ]; then
+    echo "🏗️ Building demo app..."
+    yarn build
+
+    echo "🚀 Starting dev server..."
+    yarn dev
+fi
+
+if [ "$choice" == "vercel" ]; then
+
+    # check if vercel is installed
+    if ! command -v vercel &> /dev/null; then
+        echo "Vercel is not installed. Please install it from https://vercel.com/docs/cli"
+        exit 1
+    fi
+
+    echo "🔗 Linking demo app to Vercel..."
+    vercel link --yes --scope $VERCEL_SCOPE
+    vercel pull --yes --scope $VERCEL_SCOPE
+
+    echo "🔗 vercel build"
+    vercel build
+    
+    echo "🚀 Deploying Preview on Vercel..."
+    vercel --prebuilt
+fi
