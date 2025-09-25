96763c63680d276def6faf605465282807308c53
Coleman McFarland <43583445+dontlaugh@users.noreply.github.com>
2025-09-25 15:49:55 -0400

    Extract func, tests for filtering usage-based entitlements (#617)
    * Extract func, tests for filtering usage-based entitlements
    
    * Move function and tests to src/utils/api

--- components/src/utils/api/entitlement.ts
+++ components/src/utils/api/entitlement.ts
@@ -4,7 +4,12 @@ import {
   type PlanEntitlementResponseData,
 } from "../../api/checkoutexternal";
 import { FeatureType, PriceBehavior } from "../../const";
-import type { Credit, Entitlement, PriceTier } from "../../types";
+import type {
+  Credit,
+  CurrentUsageBasedEntitlement,
+  Entitlement,
+  PriceTier,
+} from "../../types";
 import { getEntitlementCost } from "../../utils";
 
 const PeriodName: Record<string, string | undefined> = {
@@ -157,4 +162,28 @@ export function getCreditBasedEntitlementLimit(
       period: matchedCredit.period,
     };
   }
+}
+
+export function extractCurrentUsageBasedEntitlements(
+  features: Array<FeatureUsageResponseData> = [],
+  planPeriod: string,
+): CurrentUsageBasedEntitlement[] {
+  return features.reduce((acc: CurrentUsageBasedEntitlement[], entitlement) => {
+    if (
+      entitlement.priceBehavior &&
+      ((planPeriod === "month" && entitlement.monthlyUsageBasedPrice) ||
+        (planPeriod === "year" && entitlement.yearlyUsageBasedPrice))
+    ) {
+      const allocation = entitlement.allocation || 0;
+      const usage = entitlement.usage || 0;
+      const quantity = allocation ?? usage;
+      acc.push({
+        ...entitlement,
+        allocation,
+        usage,
+        quantity,
+      });
+    }
+    return acc;
+  }, []);
 }
