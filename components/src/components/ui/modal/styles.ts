c7d36bab0c4243660837cad98559427f41170709
Joseph Chrzan <cole@schematichq.com>
2025-11-04 17:25:07 -0500

    `Modal` pushed down when body has relative position and height larger than viewport (#686)

--- components/src/components/ui/modal/styles.ts
+++ components/src/components/ui/modal/styles.ts
@@ -10,12 +10,13 @@ interface ModalProps {
 
 export const Overlay = styled(Box)`
   position: absolute;
-  top: 50%;
-  left: 50%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   z-index: 999999;
-  transform: translate(-50%, -50%);
   width: 100%;
-  height: 100%;
+  height: 100vh;
   overflow: hidden;
   scrollbar-width: thin;
   scrollbar-gutter: stable both-edges;
