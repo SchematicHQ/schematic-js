ee88900d12105b5c977b94cc0664b78bfaac0ece
Cole Chrzan <cole@schematichq.com>
2026-02-20 11:21:55 -0500

    plan manager tiered

--- components/src/utils/api/feature.ts
+++ components/src/utils/api/feature.ts
@@ -1,21 +1,29 @@
 import { type FeatureDetailResponseData } from "../../api/checkoutexternal";
 import { pluralize } from "../pluralize";
 
+/**
+ * Gets the singular or plural name of a feature
+ * @param feature
+ * @param count - optional - count from which to pluralize
+ * @param ignore - optional - ignore user-set values and use `pluralize` instead
+ */
+
 export function getFeatureName(
   feature: Pick<
     FeatureDetailResponseData,
     "name" | "singularName" | "pluralName"
   >,
   count = 0,
+  ignore = false,
 ) {
   const shouldBePlural = count === 0 || count > 1;
   const { name, singularName, pluralName } = feature;
 
-  if (pluralName && shouldBePlural) {
+  if (!ignore && pluralName && shouldBePlural) {
     return pluralName;
   }
 
-  if (singularName) {
+  if (!ignore && singularName) {
     return shouldBePlural ? pluralize(singularName, count) : singularName;
   }
 
