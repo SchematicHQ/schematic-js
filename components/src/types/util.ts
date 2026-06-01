a1aa35412a973412fbcab53cbb37132878fd458c
Ben Papillon <benpapillon@gmail.com>
2026-06-01 09:18:25 -0700

    fix(types): guard nullable entitlements and handle nullable arrays in DeepPartial

--- components/src/types/util.ts
+++ components/src/types/util.ts
@@ -1,8 +1,8 @@
 export type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends (infer U)[]
+  [K in keyof T]?: NonNullable<T[K]> extends (infer U)[]
     ? DeepPartial<U>[]
-    : T[K] extends object | undefined
-      ? DeepPartial<T[K]>
+    : NonNullable<T[K]> extends object
+      ? DeepPartial<NonNullable<T[K]>>
       : T[K];
 };
 
